fixed U250 calculation, finished VSfit function, Started CalCurve generation function

VSfit.py
@@ -9,7 +9,7 @@ Created on Fri Jan  5 14:12:30 2024
 import numpy as np
 from scipy.optimize import curve_fit
 from sklearn.metrics import r2_score
-from scipy.optimize import minimize_scalar
+from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 #%%custom colors
 orange = (244/255 , 153/255 , 62/255 , 100/100)
@@ -21,7 +21,7 @@ purplelight = (252/255 , 240/255 , 254/255 , 100/100)
 purpledark = (122/255 , 8/255 , 145/255 , 100/100)
 purpleverydark = (61/255 , 4/255 , 73/255 , 100/100)
 
-#%%functions
+#%%Support Fnctions
 def standardPlotParameters(ax):
     plt.tight_layout()
     plt.rcParams['axes.titlesize'] = 20
@@ -44,11 +44,17 @@ def DoubleSigm (x,ymax,A,a,B,b):
     y = (ymax)/((1+(A*np.exp(a*x)))*(1+(B/np.exp(b*x))))
     return y
 
-def DoubleSigmresidual(x, y_target, ymax, A, a, B, b):
-    if x < 0.1:
-        return np.inf  # Return a large positive value for negative x
-    y_predicted = DoubleSigm(x, ymax, A, a, B, b)
-    return (y_predicted - y_target)**2
+def DoubleSigmInvResidual(y_target, ymax, A, a, B, b, x_guess=50):
+    # Define the objective function to find the root of
+    objective_function = lambda x: DoubleSigm(x, ymax, A, a, B, b) - y_target
+
+    # Find the root using root_scalar
+    result = root_scalar(objective_function, bracket=[0, 200], x0=x_guess)
+
+    if result.converged:
+        return result.root
+    else:
+        raise RuntimeError("Failed to find x for the given y.")
 
 def GaussAmp(x,y0,xc,w,A):
     y = y0+A*np.exp((-((x-xc)**2))/(2*w**2))
@@ -58,7 +64,9 @@ def GaussAmpInv(y,y0,xc,w,A):
     x = np.sqrt(-2*w**2*np.log((y-y0)/A))+xc
     return x
 
-def VSfit(SignalData, TimingData, ConvertTo="U2", AcquisitionFreq=10, TunningFreq=None, StepSize=2, Range=100, FitUsed="DoubleSigm",maxFitIterations=5000, plotVS=False):
+#%%Main Function for VS fitting
+
+def VSfit(SignalData, TimingData, ConvertTo="U2", AcquisitionFreq=10, TunningFreq=None, StepSize=2, Range=100, FitUsed="DoubleSigm", maxFitIterations=5000, plotVS=False):
     
     # Set default value for TunningFreq
     if TunningFreq is None:
@@ -93,7 +101,7 @@ def VSfit(SignalData, TimingData, ConvertTo="U2", AcquisitionFreq=10, TunningFre
     valid_ConvertTo_values = ["U2", "Ekin"]
     if ConvertTo == "U2":
     # Code for "U2" conversion
-     TimingDataConverted = TimingData*StepSize*SlownessCorrectionFactor
+     TimingDataConverted = (TimingData*StepSize*SlownessCorrectionFactor)**2
     elif ConvertTo == "Ekin":
     # Code for "Ekin" conversion
         raise NotImplementedError("Not yet implemented in package.")
@@ -123,11 +131,11 @@ def VSfit(SignalData, TimingData, ConvertTo="U2", AcquisitionFreq=10, TunningFre
             IntStart = DoubleSigm(0,fit_ymax,fit_A,fit_a,fit_B,fit_b) 
             #get Intensity at at half of IntStart
             IntHalf = IntStart/2
+            
+            
             #get U2Half from IntHalf value using minimized scalar residual method
             # Perform the optimization to find the best x value
-            result = minimize_scalar(DoubleSigmresidual, args=(IntHalf, fit_ymax ,fit_A, fit_a, fit_B, fit_b))
-            # Extract the optimal x value
-            U2Half = max(result.x, 0)  # Ensure U2Half is non-negative
+            U2Half = DoubleSigmInvResidual(IntHalf,fit_ymax,fit_A,fit_a,fit_B,fit_b)
             VSResult = U2Half
             VSResultR2 = r2
         elif FitUsed == "Gauss":   
@@ -163,11 +171,20 @@ def VSfit(SignalData, TimingData, ConvertTo="U2", AcquisitionFreq=10, TunningFre
         ax.scatter(TimingDataConverted, SignalDataNormalized, label='Data',color =orange, s=50, zorder=1)
         ax.plot(TimingDataConverted, fitSignalData, label = 'fit. (r$^{2}=$'+str(round(r2,3))+')',color =purpleverydark, linewidth = 3, zorder=2)
         ax.scatter(0,IntStart,color =purpleverydark,label='100%',s=100, marker = 'H',zorder=3)
-        ax.scatter(VSResult,IntHalf,color =purpleverydark,label='50%',s=100, marker = 'h',zorder=3)       
+        ax.scatter(VSResult,IntHalf,color =purpleverydark,label='50% ('+ConvertTo+'='+str(round(VSResult,2))+')',s=100, marker = 'h',zorder=3)       
         standardPlotParameters(ax)
+        plt.ylabel('Intensity [AU]')
+        if ConvertTo == "U2":
+            plt.xlabel('$U^{2}$ [$V^{2}$]')
         
     #output VSresult
     return VSResult, VSResultR2
 
+#%%Main Function for Correlating Calcualting Sensitivities and generating Conversion functuin
+
+#Input VSresult and CalResult Data for Calibration compounds
+#plot
+#fit
+#return fit parameters
 
     