68a86b1cd33a0cdbc63003d96109430388a1d5ee
Roska <m.roska@fz-juelich.de>
2024-01-12 12:50:27 +0100

    added plotting for correlation function, labeling in PEP8, updated test code

--- VSfit.py
+++ VSfit.py
@@ -13,16 +13,16 @@ from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 #%%custom colors
 orange = (244/255 , 153/255 , 62/255 , 100/100)
-orangelight = (252/255 , 230/255 , 207/255 , 100/100)
-orangedark = (145/255 , 77/255 , 8/255 , 100/100)
-orangeverydark = (73/255 , 39/255 , 4/255 , 100/100)
+orange_light = (252/255 , 230/255 , 207/255 , 100/100)
+orange_dark = (145/255 , 77/255 , 8/255 , 100/100)
+orange_very_dark = (73/255 , 39/255 , 4/255 , 100/100)
 purple = (215/255 , 61/255 , 245/255 , 100/100)
-purplelight = (252/255 , 240/255 , 254/255 , 100/100)
-purpledark = (122/255 , 8/255 , 145/255 , 100/100)
-purpleverydark = (61/255 , 4/255 , 73/255 , 100/100)
+purple_light = (252/255 , 240/255 , 254/255 , 100/100)
+purple_dark = (122/255 , 8/255 , 145/255 , 100/100)
+purple_very_dark = (61/255 , 4/255 , 73/255 , 100/100)
 
 #%%Support Fnctions
-def standardPlotParameters(ax):
+def standard_plot_parameters(ax):
     plt.tight_layout()
     plt.rcParams['axes.titlesize'] = 20
     plt.rcParams['axes.labelsize'] = 18
@@ -40,13 +40,13 @@ def standardPlotParameters(ax):
     plt.style.use('classic')
     return
 
-def DoubleSigm (x,ymax,A,a,B,b):
-    y = (ymax)/((1+(A*np.exp(a*x)))*(1+(B/np.exp(b*x))))
+def double_sigmoid (x,y_max,fall_center,fall_slope,rise_center,rise_slope):
+    y = (y_max)/((1+(fall_center*np.exp(fall_slope*x)))*(1+(rise_center/np.exp(rise_slope*x))))
     return y
 
-def DoubleSigmInvResidual(y_target, ymax, A, a, B, b, x_guess=50):
+def double_sigmoid_find_root_scalar(y_target, y_max, fall_center, fall_slope, rise_center, rise_slope, x_guess=50):
     # Define the objective function to find the root of
-    objective_function = lambda x: DoubleSigm(x, ymax, A, a, B, b) - y_target
+    objective_function = lambda x: double_sigmoid(x, y_max, fall_center, fall_slope, rise_center, rise_slope) - y_target
 
     # Find the root using root_scalar
     result = root_scalar(objective_function, bracket=[0, 200], x0=x_guess)
@@ -56,135 +56,161 @@ def DoubleSigmInvResidual(y_target, ymax, A, a, B, b, x_guess=50):
     else:
         raise RuntimeError("Failed to find x for the given y.")
 
-def GaussAmp(x,y0,xc,w,A):
-    y = y0+A*np.exp((-((x-xc)**2))/(2*w**2))
+def gauss_amp(x,y_0,x_center,w,area):
+    y = y_0+area*np.exp((-((x-x_center)**2))/(2*w**2))
     return y
 
-def GaussAmpInv(y,y0,xc,w,A):
-    x = np.sqrt(-2*w**2*np.log((y-y0)/A))+xc
+def gauss_amp_inv(y,y_0,x_center,w,area):
+    x = np.sqrt(-2*w**2*np.log((y-y_0)/area))+x_center
     return x
+
+def sigmodial(x,y_max,center,slope):
+    y =  y_max / (1 + np.exp((x - center) / slope))
+    return y
 
 #%%Main Function for VS fitting
 
-def VSfit(SignalData, TimingData, ConvertTo="U2", AcquisitionFreq=10, TunningFreq=None, StepSize=2, Range=100, FitUsed="DoubleSigm", maxFitIterations=5000, plotVS=False):
+def vs_fit(signal_data, timing_data, convert_to="U2", acquisition_freq=10, tunning_freq=None, StepSize=2, max_voltage=100, fit_used="double_sigmoid", max_fit_iterations=5000, plot_vs=False):
     
-    # Set default value for TunningFreq
-    if TunningFreq is None:
-        TunningFreq = AcquisitionFreq
-    elif TunningFreq > AcquisitionFreq:
+    # Set default value for tunning_freq
+    if tunning_freq is None:
+        tunning_freq = acquisition_freq
+    elif tunning_freq > acquisition_freq:
         raise ValueError("VS Tunning Frequency faster than Acquisition Frequency.")
     else:
         pass
     
     #chek if timing and Signal Data have same dimension -> error
-    if SignalData.shape != TimingData.shape:
+    if signal_data.shape != timing_data.shape:
         raise ValueError("Signal and timing Data must have the same dimension.")
         
     #check if any values are negative or NaN -> error
-    if any(SignalData < 0) or any(np.isnan(SignalData)):
+    if any(signal_data < 0) or any(np.isnan(signal_data)):
         raise ValueError("Signal Data contains negative values or NaN.")
-    if any(np.isnan(TimingData)):
+    if any(np.isnan(timing_data)):
         raise ValueError("Timing Data contains NaN values.")
     
     #calcualte correction factor for if slower tunning than acquisition
-    SlownessCorrectionFactor = TunningFreq/AcquisitionFreq
+    slowness_correction_factor = tunning_freq/acquisition_freq
     
     #check if Nr of Datapoints matches Expected from VS param -> error
-    ExpectedDataPoints = Range/StepSize/SlownessCorrectionFactor
-    if ExpectedDataPoints != TimingData.shape[0]:
-        raise ValueError(f"More Data Points than expected from VS setting. {ExpectedDataPoints} expected, {TimingData.shape[0]} found")
+    expected_data_points = max_voltage/StepSize/slowness_correction_factor
+    if expected_data_points != timing_data.shape[0]:
+        raise ValueError(f"More Data Points than expected from VS setting. {expected_data_points} expected, {timing_data.shape[0]} found")
         
     #set timing Data to Start at 0
-    TimingData = TimingData-min(TimingData)
+    timing_data = timing_data-min(timing_data)
     
     #convert time (x) to U2 or Ekin
-    valid_ConvertTo_values = ["U2", "Ekin"]
-    if ConvertTo == "U2":
+    valid_convert_to_values = ["U2", "Ekin"]
+    if convert_to == "U2":
     # Code for "U2" conversion
-     TimingDataConverted = (TimingData*StepSize*SlownessCorrectionFactor)**2
-    elif ConvertTo == "Ekin":
+     timing_dataConverted = (timing_data*StepSize*slowness_correction_factor)**2
+    elif convert_to == "Ekin":
     # Code for "Ekin" conversion
         raise NotImplementedError("Not yet implemented in package.")
     else:
-        raise ValueError(f"Invalid value for ConvertTo: {ConvertTo}. Choose from {valid_ConvertTo_values}")
+        raise ValueError(f"Invalid value for convert_to: {convert_to}. Choose from {valid_convert_to_values}")
     
     #Normalize Signal (y) to highest value
-    SignalDataNormalized = SignalData/max(SignalData)
+    signal_data_normalized = signal_data/max(signal_data)
     
     try:
         #fit TS
-        valid_FitUsed_values = ["DoubleSigm", "Gauss"]
-        if FitUsed == "DoubleSigm":
+        valid_fit_used_values = ["double_sigmoid", "Gauss"]
+        if fit_used == "double_sigmoid":
             #set boundary conditions 
-            lowBounds = (0,0,0,0,0)
-            upBounds = (np.inf,np.inf,np.inf,np.inf,np.inf)
-            #perform DoubleSigm fit
-            parameters, covariance = curve_fit(DoubleSigm, TimingDataConverted, SignalDataNormalized, maxfev=maxFitIterations, bounds= (lowBounds,upBounds))
+            low_bounds = (0,0,0,0,0)
+            up_bounds = (np.inf,np.inf,np.inf,np.inf,np.inf)
+            #perform double_sigmoid fit
+            parameters, covariance = curve_fit(double_sigmoid, timing_dataConverted, signal_data_normalized, maxfev=max_fit_iterations, bounds= (low_bounds,up_bounds))
             #unpack parameters
-            fit_ymax, fit_A, fit_a, fit_B, fit_b = parameters  
+            fit_y_max,fit_fall_center, fit_fall_slop, rise_center, rise_slope = parameters  
             #calculate fit curve
-            fitSignalData = DoubleSigm(TimingDataConverted,fit_ymax,fit_A,fit_a,fit_B,fit_b)
+            fit_signal_data = double_sigmoid(timing_dataConverted,fit_y_max,fit_fall_center, fit_fall_slop, rise_center, rise_slope)
             #r2 value
             #r-squared value between data and fit curve
-            r2 = r2_score(SignalDataNormalized,fitSignalData)
-            #get start value from SignalData
-            IntStart = DoubleSigm(0,fit_ymax,fit_A,fit_a,fit_B,fit_b) 
-            #get Intensity at at half of IntStart
-            IntHalf = IntStart/2
-            
-            
-            #get U2Half from IntHalf value using minimized scalar residual method
+            r2 = r2_score(signal_data_normalized,fit_signal_data)
+            #get start value from signal_data
+            int_start = double_sigmoid(0,fit_y_max,fit_fall_center, fit_fall_slop, rise_center, rise_slope) 
+            #get Intensity at at half of int_start
+            int_half = int_start/2
+            #get u2_half from int_half value using minimized scalar residual method
             # Perform the optimization to find the best x value
-            U2Half = DoubleSigmInvResidual(IntHalf,fit_ymax,fit_A,fit_a,fit_B,fit_b)
-            VSResult = U2Half
-            VSResultR2 = r2
-        elif FitUsed == "Gauss":   
+            u2_half = double_sigmoid_find_root_scalar(int_half,fit_y_max,fit_fall_center, fit_fall_slop, rise_center, rise_slope)
+            vs_result = u2_half
+            vs_result_r2 = r2
+        elif fit_used == "Gauss":   
             #set boundary conditions 
-            lowBounds = (-np.inf,-np.inf,-np.inf,-np.inf)
-            upBounds = (np.inf,np.inf,np.inf,np.inf)
-            #perform DoubleSigm fit
-            parameters, covariance = curve_fit(GaussAmp, TimingDataConverted, SignalDataNormalized, maxfev=maxFitIterations, bounds= (lowBounds,upBounds))
+            low_bounds = (-np.inf,-np.inf,-np.inf,-np.inf)
+            up_bounds = (np.inf,np.inf,np.inf,np.inf)
+            #perform double_sigmoid fit
+            parameters, covariance = curve_fit(gauss_amp, timing_dataConverted, signal_data_normalized, maxfev=max_fit_iterations, bounds= (low_bounds,up_bounds))
             #unpack parameters
-            fit_y0, fit_xc, fit_w, fit_A = parameters  
+            fit_y_0, fit_x_center, fit_w, fit_area = parameters  
             #calculate fit curve
-            fitSignalData = GaussAmp(TimingDataConverted, fit_y0, fit_xc, fit_w, fit_A)  
+            fit_signal_data = gauss_amp(timing_dataConverted, fit_y_0, fit_x_center, fit_w, fit_area)
             #r2 value
             #r-squared value between data and fit curve
-            r2 = r2_score(SignalDataNormalized,fitSignalData)
-            #get start value from SignalData
-            IntStart = GaussAmp(0, fit_y0, fit_xc, fit_w, fit_A) 
-            #get Intensity at at half of IntStart
-            IntHalf = IntStart/2
-            #get EkinHalf from IntHalf value using minimized scalar residual method
-            EkinHalf = GaussAmpInv(IntHalf, fit_y0, fit_xc, fit_w, fit_A)
-            VSResult = EkinHalf
-            VSResultR2 = r2
+            r2 = r2_score(signal_data_normalized,fit_signal_data)
+            #get start value from signal_data
+            int_start = gauss_amp(0, fit_y_0, fit_x_center, fit_w, fit_area) 
+            #get Intensity at at half of int_start
+            int_half = int_start/2
+            #get ekin_half from int_half value using minimized scalar residual method
+            ekin_half = gauss_amp_inv(int_half, fit_y_0, fit_x_center, fit_w, fit_area)
+            vs_result = ekin_half
+            vs_result_r2 = r2
         else:
-            raise ValueError(f"Invalid value for FitUsed: {FitUsed}. Choose from {valid_FitUsed_values}")
+            raise ValueError(f"Invalid value for fit_used: {fit_used}. Choose from {valid_fit_used_values}")
 
     except (RuntimeError, RuntimeWarning, ValueError) as e:
         raise RuntimeError("fit not possible") from e
     
     #plot VS
-    if plotVS == True:
+    if plot_vs == True:
         fig, ax = plt.subplots(figsize=(5.5,4.125))
-        ax.scatter(TimingDataConverted, SignalDataNormalized, label='Data',color =orange, s=50, zorder=1)
-        ax.plot(TimingDataConverted, fitSignalData, label = 'fit. (r$^{2}=$'+str(round(r2,3))+')',color =purpleverydark, linewidth = 3, zorder=2)
-        ax.scatter(0,IntStart,color =purpleverydark,label='100%',s=100, marker = 'H',zorder=3)
-        ax.scatter(VSResult,IntHalf,color =purpleverydark,label='50% ('+ConvertTo+'='+str(round(VSResult,2))+')',s=100, marker = 'h',zorder=3)       
-        standardPlotParameters(ax)
+        ax.scatter(timing_dataConverted, signal_data_normalized, label='Data',color =orange, s=50, zorder=1)
+        ax.plot(timing_dataConverted, fit_signal_data, label = 'fit. (r$^{2}=$'+str(round(r2,3))+')',color =purple_very_dark, linewidth = 3, zorder=2)
+        ax.scatter(0,int_start,color =purple_very_dark,label='100%',s=100, marker = 'H',zorder=3)
+        ax.scatter(vs_result,int_half,color =purple_very_dark,label='50% ('+convert_to+'='+str(round(vs_result,2))+')',s=100, marker = 'h',zorder=3)       
+        standard_plot_parameters(ax)
         plt.ylabel('Intensity [AU]')
-        if ConvertTo == "U2":
+        if convert_to == "U2":
             plt.xlabel('$U^{2}$ [$V^{2}$]')
         
-    #output VSresult
-    return VSResult, VSResultR2
+    #output vs_result
+    return vs_result, vs_result_r2
 
 #%%Main Function for Correlating Calcualting Sensitivities and generating Conversion functuin
 
-#Input VSresult and CalResult Data for Calibration compounds
-#plot
-#fit
-#return fit parameters
-
-    
+#Input vs_result and CalResult Data and Names for Calibration compounds
+def generate_calibration_curve(CalVSValues, CalSensValues, plotCalCurve=False, ConvertedTo="U2"):
+    #set boundary conditions 
+    low_bounds = (0,-np.inf,-np.inf)
+    up_bounds = (np.inf,np.inf,0)
+    #perform Sigm fit
+    parameters, covariance = curve_fit(sigmodial, CalVSValues, CalSensValues, maxfev=10000, bounds= (low_bounds,up_bounds))
+    #unpack parameters
+    fit_y_max, fit_center, fit_slope = parameters
+    #calculate fit curve
+    fitSensData = sigmodial(CalVSValues, fit_y_max, fit_center, fit_slope)
+    #r2 value
+    #r-squared value between data and fit curve
+    CalCurver2 = r2_score(CalSensValues,fitSensData)
+    #plot
+    if plotCalCurve == True:
+        #generate plot x dataaxis Dataset
+        PlotFitVSValues = np.linspace(0, max(CalVSValues)+1.1,1000)
+        PlotFitSensData = sigmodial(PlotFitVSValues,fit_y_max, fit_center, fit_slope)
+        fig, ax = plt.subplots(figsize=(5.5,3.44))
+        ax.scatter(CalVSValues, CalSensValues, c = orange, edgecolor =  'None' ,s=60,zorder=2,label = 'cal. compounds')
+        ax.plot(PlotFitVSValues, PlotFitSensData, label= 'sigm. fit (r$^{2}=$'+str(round(CalCurver2,3))+')',color =purple, linewidth = 1, zorder=3)
+        standard_plot_parameters(ax)
+        plt.ylabel('Sensitivity')
+        if ConvertedTo == "U2":
+            plt.xlabel('$U^{2}$ [$V^{2}$]')
+        plt.rcParams['legend.fontsize'] = 10
+        plt.legend(loc='best',handlelength=1)
+    #output fit parameters 
+    return parameters, CalCurver2 
--- testVSfit.py
+++ testVSfit.py
@@ -5,15 +5,26 @@ Created on Wed Jan 10 15:09:12 2024
 @author: m.roska
 """
 #%%
-from VSfit import VSfit
+from VSfit import vs_fit, generate_calibration_curve, sigmodial
 import pandas as pd
 #%%
 # Load the CSV file into a DataFrame
 df = pd.read_csv('C://Users/m.roska/OneDrive - Forschungszentrum Jülich GmbH/Desktop/VSExampleData.csv')
 df = df[:-15]
 
-TimingData = df['xVS']
-SignalData = df['yVS']
+timing_data = df['xVS']
+signal_data = df['yVS']
 
 #%%
-VSResult, VSResultR2 = VSfit(SignalData, TimingData, plotVS=True)
+vs_result, vs_result_r2 = vs_fit(signal_data, timing_data, plot_vs=True)
+
+#%% test correlation function
+
+u2 = [0,10,20,30,40,50,60,70,80]
+sens = [1,1, 1, 4, 6, 8,9,9,9]
+
+parameters, r2 = generate_calibration_curve(u2,sens,plotCalCurve=True)
+
+#%% use sigmoidal to convert to sens
+fit_y_max, fit_center, fit_slope = parameters
+sen_rResult = sigmodial(vs_result, fit_y_max, fit_center, fit_slope)
